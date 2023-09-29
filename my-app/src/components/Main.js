80ae3e6fce0809bb81b4d5440939daa4c18e498c
luyvannda <philips.joe329@gmail.com>
2023-09-29 15:04:04 +0700

    add html texts to main.js

--- my-app/src/components/Main.js
+++ my-app/src/components/Main.js
@@ -1,7 +1,20 @@
+import logo from "./logo.svg"
+
 export default function Main() {
   return (
-    <div>
-      <h2>I'm the main contents</h2>
+    <div className="main-container">
+      <div className="main-left">
+        <h2>Fun facts about React</h2>
+        <ul>
+          <li>Was first released in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 100k stars on GitHub</li>
+          <li>Is maintained by Facebook</li>
+          <li>Powers thousands of enterprise apps, including
+            mobile apps</li>
+        </ul>
+      </div>
+      <img className="main-logo-right" src={logo} alt="A React logo hidden in half"></img>
     </div>
   )
 }

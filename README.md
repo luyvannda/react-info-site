a90feda2382d1e0d87dfa334a474767d50211e4c
luyvannda <philips.joe329@gmail.com>
2023-09-29 10:42:29 +0700

    complete the nav bar component

--- README.md
+++ README.md
@@ -6,4 +6,3 @@ Description:
 
 This is the first React project site I will be learning to build with the help of video lessons from Scrimba.
 
-Hello just testing if git is working or not?
--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,7 +1,15 @@
-
+import logo from "./logo.svg"
 
 export default function Navbar() {
   return (
-    <h1>Navbar goes here!!!</h1>
+    <div className="nav-container">
+      <div className="nav-left">
+        <img className="nav-logo" src={logo} alt="A react logo"></img>
+        <h1>ReactFacts</h1>
+      </div>
+      <div className="nav-right">
+        <p>React Course - Project 1</p>
+      </div>
+    </div>
   )
 }
